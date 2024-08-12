d0f41cd7fb26392c1c914dbb05924049033a5115
MorrisLin <aka611511@gmail.com>
2024-08-12 22:04:10 +0800

    feat: refactor home vue, and NavBar

---
+++ src/components/MenuCard.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+  to: {
+    type: String,
+    required: true
+  },
+  styleClass: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <RouterLink :to="to" :class="styleClass">
+    <slot></slot>
+  </RouterLink>
+</template>
